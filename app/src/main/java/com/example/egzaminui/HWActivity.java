74241d266205015ecc48d496586a552925bf595f
Jolanta <baleviciutejolanta@gmail.com>
2024-06-03 22:17:34 +0300

    MainActivity of the HW

--- app/src/main/java/com/example/egzaminui/HWActivity.java
+++ app/src/main/java/com/example/egzaminui/HWActivity.java
@@ -2,13 +2,33 @@ package com.example.egzaminui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class HWActivity extends AppCompatActivity {
+
+    private ListView listView;
+    private MyListAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hwactivity);
+
+        listView = (ListView) findViewById(R.id.list);
+        ArrayList<Item> myList = new ArrayList<Item>();
+        myList.add(new Item("SERIAL:", Build.getSerial()));
+        myList.add(new Item("MODEL:", Build.MODEL));
+        myList.add(new Item("ID:", Build.ID));
+        myList.add(new Item("MANUFACTURE:", Build.MANUFACTURER));
+        myList.add(new Item("BRAND:", Build.BRAND));
+        // And so on:)
+
+        mAdapter = new MyListAdapter(this, R.layout.item_main, myList);
+        listView.setAdapter(mAdapter);
+        setTitle("Info");
     }
 }
---
+++ app/src/main/java/com/example/egzaminui/MyListAdapter.java
@@ -0,0 +1,41 @@
+package com.example.egzaminui;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+public class MyListAdapter extends ArrayAdapter<Item> {
+
+    private int resourceLayout;
+    private Context context;
+
+    public MyListAdapter(Context context, int resource, List<Item> items) {
+        super(context, resource, items);
+        this.resourceLayout = resource;
+        this.mContext = context;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View v = convertView;
+        if (v == null)
+            v = LayoutInflater.from(mContext).inflate(resourceLayout, parent, false);
+
+        Item p = getItem(position);
+
+        if (p != null) {
+            TextView titleTextView = (TextView) v.findViewById(R.id.titleTextView);
+            TextView valueTextView = (TextView) v.findViewById(R.id.valueTextView);
+
+            if (titleTextView != null)
+                titleTextView.setText(p.getTitle());
+
+            if (valueTextView != null)
+                valueTextView.setText(p.getValue());
+        }
+        return v;
+    }
+}
