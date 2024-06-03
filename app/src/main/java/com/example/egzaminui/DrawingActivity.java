29021840900f6d62bc1b1bd6123587f518756c04
Jolanta <baleviciutejolanta@gmail.com>
2024-06-03 22:47:26 +0300

    Reapply "Merge branch 'main' into Lecture8"
    This reverts commit 531d0b3b753dabf72570c331eb20a68836166172.

---
+++ app/src/main/java/com/example/egzaminui/DrawingActivity.java
@@ -0,0 +1,86 @@
+package com.example.egzaminui;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+
+public class DrawingActivity extends AppCompatActivity {
+
+    private DrawView drawView;
+    private Button triangleBtn;
+    private Button circleBtn;
+    private Button rectangleBtn;
+    private Button clearBtn;
+    private CheckBox fillFlagCB;
+
+    private Boolean fillFlag = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drawing);
+
+        drawView = (DrawView) findViewById(R.id.draw_view);
+
+        triangleBtn = (Button) findViewById(R.id.triangle_btn);
+        circleBtn = (Button) findViewById(R.id.circle_btn);
+        rectangleBtn = (Button) findViewById(R.id.rectangle_btn);
+        clearBtn = (Button) findViewById(R.id.clear_btn);
+        fillFlagCB = (CheckBox) findViewById(R.id.fill_flag_cb);
+
+        triangleBtn.setOnClickListener(triangleBtnClick);
+        circleBtn.setOnClickListener(circleBtnClick);
+        rectangleBtn.setOnClickListener(rectangleBtnClick);
+        clearBtn.setOnClickListener(clearBtnClick);
+        //fillFlagCB.setOnClickListener(fillFlagCBChange);
+    }
+
+    public void setFigure(final int figure, final boolean fillFlag) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                drawView.setFigure(figure);
+                drawView.setFillFlag(fillFlag);
+                drawView.invalidate();
+            }
+        });
+    }
+
+    CompoundButton.OnCheckedChangeListener fillFlagCBChange = new CompoundButton.OnCheckedChangeListener() {
+        @Override
+        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+            fillFlag = isChecked;
+        }
+    };
+
+    View.OnClickListener triangleBtnClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            setFigure(1, fillFlag);
+        }
+    };
+
+    View.OnClickListener circleBtnClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            setFigure(2, fillFlag);
+        }
+    };
+
+    View.OnClickListener rectangleBtnClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            setFigure(3, fillFlag);
+        }
+    };
+
+    View.OnClickListener clearBtnClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            setFigure(0, fillFlag);
+        }
+    };
+}
