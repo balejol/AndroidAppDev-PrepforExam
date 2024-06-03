1a50d45baaaf68074b76e6f76bfc7df410eda5a4
Jolanta <baleviciutejolanta@gmail.com>
2024-06-03 21:19:48 +0300

    Class OpenGLView

--- app/src/main/java/com/example/egzaminui/OpenGLRender.java
+++ app/src/main/java/com/example/egzaminui/OpenGLRender.java
@@ -1,6 +1,8 @@
 package com.example.egzaminui;
 
 import android.opengl.GLES30;
+import android.opengl.Matrix;
+import android.os.SystemClock;
 
 public class OpenGLRender implements GLSurfaceView.Renderer {
 
@@ -16,17 +18,33 @@ public class OpenGLRender implements GLSurfaceView.Renderer {
 
     @Override
     public void onSurfaceCreated(GL10 gl10, EGLConfig eglConfig) {
-
+        GLES30.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        Triangle = new Triangle();
     }
 
     @Override
     public void onDrawFrame(GL10 gl10) {
+        GLES30.glClear(GLES30.GL_COLOR_BUFFER_BIT);
+        Matrix.setLookAtM(ViewMatrix, 0,0,0,-3,0f,0f,0f,0f,1.0f,0.0f);
 
+        if (getAnimationFlag() == true) {
+            long time = SystemClock.uptimeMillis() % 4000L;
+            float angle = 0.090f * ((int) time);
+            Angle angle;
+        }
+
+        Matrix.setRotateM(MMatrix, 0, Angle,0,0,1.0f);
+        Matrix.multiplyMM(MVPMatrix,0,ViewMatrix,0,MMatrix,0);
+        Matrix.multiplyMM(MVPMatrix,0,ProjectionMatrix,0,MVPMatrix,0);
+
+        Triangle.draw(MVPMatrix);
     }
 
     @Override
-    public void onSurfaceChandeg(GL10 gl10, int width, int height) {
-
+    public void onSurfaceChanged(GL10 gl10, int width, int height) {
+        GLES30.glViewport(0, 0, width, height);
+        float ratio = (float) width / height;
+        Matrix.frustumM(ProjectionMatrix, 0, -ratio, ratio, -1, 1, 3, 7);
     }
 
     public static int loadShader(int typer, String shaderCode) {
@@ -51,4 +69,6 @@ public class OpenGLRender implements GLSurfaceView.Renderer {
     public boolean getAnimationFlag() {
         return animationFlag;
     }
+
+    private Triangle Triangle;
 }
