d6b00bedc505c47283b0b76af84e456f9ce99335
Jolanta <baleviciutejolanta@gmail.com>
2024-06-03 22:03:33 +0300

    Item class

---
+++ app/src/main/java/com/example/egzaminui/Item.java
@@ -0,0 +1,17 @@
+package com.example.egzaminui;
+
+public class Item {
+
+    private String title;
+    private String value;
+
+    public Item(String title, String value) {
+        this.title = title;
+        this.value = value;
+    }
+
+    public void setTitle(String title){ this.title = title; }
+    public String getTitle(){ return title; }
+    public void setValue(){ this.value = value; }
+    public String getValue(){ return value; }
+}
