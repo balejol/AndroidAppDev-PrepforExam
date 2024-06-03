1530a8fdd00707e0662e6fc15cf8aa55b2438bed
Jolanta <baleviciutejolanta@gmail.com>
2024-06-03 22:00:43 +0300

    xml

---
+++ app/src/main/java/com/example/egzaminui/HWActivity.java
@@ -0,0 +1,14 @@
+package com.example.egzaminui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HWActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hwactivity);
+    }
+}
